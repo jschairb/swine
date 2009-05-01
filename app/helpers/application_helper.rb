057719475d5f7931d9e8152b9ed8bb1c9bd99faf
Josh Schairbaum <joshua.schairbaum@gmail.com>
2009-05-01 15:22:43 -0400

    added authority for authoritative tweeters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  AUTHORITY = %w(cdcemergency whonews healthmap)
+
+  def tweet_class(user)
+    status = "tweet"
+    status << " authority" if AUTHORITY.include?(user)
+    return status
+  end
 end
