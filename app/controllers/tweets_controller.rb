811df2d6f537b66f66028051f4515ad0fe39a330
Josh Schairbaum <joshua.schairbaum@gmail.com>
2009-05-04 17:43:59 -0400

    adding endless paging

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,14 @@
 class TweetsController < ApplicationController
+
   def index
     @tweets = Tweet.paginate( :page => params[:page],
                               :order => 'status_created_at DESC',
-                              :per_page => 50 )
+                              :per_page => 15 )
     @tweets_count = Tweet.count(:all)
+
+    if params[:without_layout]
+      render :partial => "tweets", :locals => {:tweets => @tweets}
+    end
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,19 @@ module ApplicationHelper
     status << " authority" if AUTHORITY.include?(user)
     return status
   end
+
+  def pageless(total_pages, url=nil)
+    javascript_tag(
+      "$('#tweets_list').pageless({" \
+        "totalPages:#{total_pages}," \
+        "url:'#{url}'," \
+        "params:{without_layout: true}, " \
+        "loaderMsg:'Loading more results'" \
+      "});"
+    )
+  end
+
+  def time_ago_dates
+    javascript_tag("$('abbr[class*=timeago]').timeago();")
+  end
 end
